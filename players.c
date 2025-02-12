b3549247665802c87955ffbee5ec31e96c2cb712
OTTimi <150872598+OTTimi@users.noreply.github.com>
2025-02-12 09:22:12 -0500

    Final Update players.c

--- players.c
+++ players.c
@@ -14,11 +14,9 @@
 #include "players.h"
 
 // Returns true if the player name matches one of the existing players
-bool player_exists(player *players, int num_players, char *name)
-{
+bool player_exists(player *players, int num_players, char *name) {
     for (int i = 0; i < num_players; i++) {
-        // Ensure name is not NULL before comparing
-        if (players[i].name[0] != '\0' && strcmp(players[i].name, name) == 0) {
+        if (strcmp(players[i].name, name) == 0) {
             return true;
         }
     }
@@ -27,12 +25,11 @@ bool player_exists(player *players, int num_players, char *name)
 
 // Go through the list of players and update the score for the 
 // player given their name
-void update_score(player *players, int num_players, char *name, int score)
-{
+void update_score(player *players, int num_players, char *name, int score) {
     for (int i = 0; i < num_players; i++) {
-        if (strcmp(players[i].name, name) == 0) {  // Fixed: using 0 instead of 'o'
+        if (strcmp(players[i].name, name) == 0) {
             players[i].score += score;
-            break; // Exit loop after updating the score
+            break;
         }
     }
 }
